48642842d8da0040d81d237a2ef0a982a065a8ca
Abner Joia <t2m.ajoia@terceiros.mag.com.br>
2022-05-21 12:20:02 -0300

    Projeto inicial

---
+++ ClienteScore.MAG/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+
+namespace ClienteScore.MAG
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
